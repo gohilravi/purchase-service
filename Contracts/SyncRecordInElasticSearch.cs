03a13d018f2e80515b20f132348f3bb9c5790886
Purvish Chandarana <purvish.chandarana@marutitech.com>
2026-01-02 20:28:31 +0530

    Update Event contracts

---
+++ Contracts/SyncRecordInElasticSearch.cs
@@ -0,0 +1,9 @@
+﻿namespace purchase_service.Contracts;
+
+public class SyncRecordInElasticSearch
+{
+    public string ElasticSearchId { get; set; } = string.Empty;
+    public string ObjectType { get; set; } = "Purchase";
+    public string Operation { get; set; } = string.Empty;
+    public string Payload { get; set; } = string.Empty;
+}
